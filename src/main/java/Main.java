3139fe47e09c23bb60d1313b803de0d90989844c
t.popov <gus.love.belarus@gmail.com>
2020-10-21 22:12:25 +0300

    Second commit and push

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args){
         System.out.println("Класс мэйн - первая строка кода");
         System.out.println("Класс мэйн - первый комит");
+        System.out.println("Класс мэйн - второй коммит, первый ещё не пушнут");
 
 
     }
--- src/main/java/NewClass.java
+++ src/main/java/NewClass.java
@@ -1,3 +1,4 @@
 public class NewClass {
     System.out.println("Класс ньюкласс - первый комит");
+    System.out.println("Класс newclass - второй коммит, первый ещё не пушнут");
 }
