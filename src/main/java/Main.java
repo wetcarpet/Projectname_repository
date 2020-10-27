6f773f96b8d0b6c05c9ea111ea4020af2d12fd57
wetcarpet <65464973+wetcarpet@users.noreply.github.com>
2020-10-27 22:15:47 +0300

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,6 +4,7 @@ public class Main {
         System.out.println("Класс мэйн - первый комит");
         System.out.println("Класс мэйн - второй коммит, первый ещё не пушнут");
         System.out.println("Новый нон-ьастер бранч1");
+        System.out.println("вытягиваю из нон-мастера1");
 
 
     }
